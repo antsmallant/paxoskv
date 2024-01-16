26eaac6b7558e91b29e9fe8ed549eb598fe453d4
antsmallant <412288482@qq.com>
2024-01-16 11:47:49 +0800

    use $HOME

--- install-protoc.sh
+++ install-protoc.sh
@@ -5,7 +5,7 @@ PROTOC_FILENAME=protoc-${PROTOBUF_VERSION}-linux-x86_64.zip
 
 (
 
-cd /home/travis
+cd $HOME
 
 wget https://github.com/google/protobuf/releases/download/v$PROTOBUF_VERSION/$PROTOC_FILENAME
 unzip $PROTOC_FILENAME
